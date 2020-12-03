23976947994351aa738872ddb5963e066bbbcbfd
isabelchua <isabelchua86@gmail.com>
2020-12-03 00:51:04 -0800

    add list

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -8,6 +8,26 @@ const url = "https://course-api.com/react-tours-project";
 function App() {
 	const [loading, setLoading] = useState(true);
 	const [tours, setTours] = useState([]);
+
+	const fetchTours = async () => {
+		setLoading(true);
+
+		try {
+			const response = await fetch(url);
+			const tours = await response.json();
+			setLoading(false);
+			setTours(tours);
+		} catch (error) {
+			setLoading(false);
+			console.log(error);
+		}
+
+		console.log(tours);
+	};
+
+	useEffect(() => {
+		fetchTours();
+	}, []);
 	if (loading) {
 		return (
 			<main>
@@ -16,7 +36,11 @@ function App() {
 		);
 	}
 
-	return <h2>Tours Project Setup</h2>;
+	return (
+		<main>
+			<Tours tours={tours} />
+		</main>
+	);
 }
 
 export default App;
