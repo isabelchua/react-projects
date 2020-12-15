95f34d4a05f380d0c99a3c09cbb10ff75f625a5f
isabelchua <isabelchua86@gmail.com>
2020-12-15 17:31:58 +0800

    done

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -37,7 +37,11 @@ function App() {
 				<div className="btn-container">
 					{jobs.map((item, index) => {
 						return (
-							<button key={item.id} onClick={() => setValue(index)}>
+							<button
+								key={item.id}
+								onClick={() => setValue(index)}
+								className={`job-btn ${index === value && "active-btn"}`}
+							>
 								{item.company}
 							</button>
 						);
