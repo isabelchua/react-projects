846c489644a748f90deb557a1564af3ae0924edc
isabelchua <isabelchua86@gmail.com>
2020-12-16 20:18:30 +0800

    done

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -6,6 +6,23 @@ function App() {
 	const [people, setPeople] = useState(data);
 	const [index, setIndex] = useState(0);
 
+	useEffect(() => {
+		const lastIndex = people.length - 1;
+		if (index < 0) {
+			setIndex(lastIndex);
+		}
+		if (index > lastIndex) {
+			setIndex(0);
+		}
+	}, [index, people]);
+
+	useEffect(() => {
+		let slider = setInterval(() => {
+			setIndex(index + 1);
+		}, 3000);
+		return () => clearInterval(slider);
+	}, [index]);
+
 	return (
 		<section className="section">
 			<div className="title">
@@ -40,8 +57,8 @@ function App() {
 				<button className="prev" onClick={() => setIndex(index - 1)}>
 					<FiChevronLeft />
 				</button>
-				<button className="prev">
-					<FiChevronLeft onClick={() => setIndex(index + 1)} />
+				<button className="next" onClick={() => setIndex(index + 1)}>
+					<FiChevronRight />
 				</button>
 			</div>
 		</section>
