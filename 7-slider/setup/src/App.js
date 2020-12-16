73c830d79ebc4031059b2db4188f477e08a00b19
isabelchua <isabelchua86@gmail.com>
2020-12-16 10:49:17 +0800

    initial commit

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -1,9 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 function App() {
-  return <h2>slider project setup</h2>;
+	const [people, setPeople] = useState(data);
+	const [index, setIndex] = useState(0);
+
+	return (
+		<section className="section">
+			<div className="title">
+				<span>/</span>reviews
+			</div>
+			<div className="section-center">
+				{people.map((person, personIndex) => {
+					const { id, image, name, title, quote } = person;
+					//more stuff
+
+					return (
+						<article>
+							<img src={image} alt={name} className="person-img" />
+						</article>
+					);
+				})}
+			</div>
+		</section>
+	);
 }
 
 export default App;
