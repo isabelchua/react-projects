3464ac36eda213194ba50315e4cb261874da320a
isabelchua <isabelchua86@gmail.com>
2020-12-22 20:03:30 +0800

    loading

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">about us</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur
+				dignissimos architecto maiores? Harum recusandae voluptatem ab.
+				Libero quis temporibus vitae quam, dignissimos reiciendis
+				praesentium nihil inventore excepturi accusamus a alias!
+			</p>
+		</section>
+	);
+};
 
-export default About
+export default About;
