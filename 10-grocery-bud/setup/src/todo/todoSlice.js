22cb6c558fe0a980e3c809b05e7cde5704a40de6
isabelchua <isabelchua86@gmail.com>
2021-01-04 22:26:06 +0800

    delete added

--- 10-grocery-bud/setup/src/todo/todoSlice.js
+++ 10-grocery-bud/setup/src/todo/todoSlice.js
@@ -10,22 +10,25 @@ export const todoSlice = createSlice({
 		2: { id: 2, task: "eat" }
 	},
 	reducers: {
-		setTodo: (state, action) => {
-			// Redux Toolkit allows us to write "mutating" logic in reducers. It
-			// doesn't actually mutate the state because it uses the Immer library,
-			// which detects changes to a "draft state" and produces a brand new
-			// immutable state based off those changes
-			state.todo = action.payload.todo;
-		},
 		deleteTodo: (state, action) => {
-			console.log(action.payload.id);
-			console.log(state);
-			//state.filter(item => item.id !== action.payload.id);
+			delete state[action.payload.id];
 		}
 	}
 });
 
 export const { setTodo, logout, deleteTodo } = todoSlice.actions;
+
+// state.todo = state.todo.filter(item => item.id !== action.payload.id);
+//initialState: [{ id: 1, task: "gym" }, { id: 2, task: "eat" }],
+// setTodo: (state, action) => {
+// 	// Redux Toolkit allows us to write "mutating" logic in reducers. It
+// 	// doesn't actually mutate the state because it uses the Immer library,
+// 	// which detects changes to a "draft state" and produces a brand new
+// 	// immutable state based off those changes
+// 	state.todo = action.payload.todo;
+// },
+// console.log(action.payload.id);
+// 			console.log(action.payload);
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
