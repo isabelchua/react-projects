2bfd897f4b40dbc325d87e7be689f6a1ab2c6606
isabelchua <isabelchua86@gmail.com>
2021-01-06 19:15:56 +0800

    card item

--- react-responsive/src/components/Cards.js
+++ react-responsive/src/components/Cards.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CardItem from "./CardItem";
-
+import "./Cards.css";
 const Cards = () => {
 	return (
 		<div className="cards">
@@ -8,7 +8,12 @@ const Cards = () => {
 			<div className="cards__container">
 				<div className="cards__wrapper">
 					<ul className="cards__items">
-						<CardItem />
+						<CardItem
+							src="images/img-9.jpg"
+							text="Explore the hidden waterfall deep inside the Amazon jungle"
+							label="Adventure"
+							path="./services"
+						/>
 					</ul>
 				</div>
 			</div>
