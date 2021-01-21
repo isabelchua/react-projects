87103d5f94e895254ee77d8d5f95d65eb0d5fb60
isabelchua <isabelchua86@gmail.com>
2021-01-21 20:58:42 +0800

    instances

--- axios-crash-course/src/main.js
+++ axios-crash-course/src/main.js
@@ -154,7 +154,7 @@ function cancelToken() {
 		});
 
 	if (true) {
-		source.cancel("Request canceled");
+		source.cancel("Request canceled!");
 	}
 }
 
@@ -175,6 +175,11 @@ axios.interceptors.request.use(
 );
 
 // AXIOS INSTANCES
+const axiosInstance = axios.create({
+	baseURL: "https://jsonplaceholder.typicode.com"
+});
+
+axiosInstance.get("/comments").then(res => showOutput(res));
 
 // Show output in browser
 function showOutput(res) {
