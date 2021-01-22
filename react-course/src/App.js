4fa6196987af98a87d4a5968c5332999e66c9579
isabelchua <isabelchua86@gmail.com>
2021-01-22 21:05:07 +0800

    css

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-	return <div className="App">hi</div>;
+	return (
+		<div className="container">
+			<Header />
+			hi
+		</div>
+	);
 }
 
 export default App;
