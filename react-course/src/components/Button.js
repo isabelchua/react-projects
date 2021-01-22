10ebb8ac39de7ed1a1c532998102cada08cf1fb6
isabelchua <isabelchua86@gmail.com>
2021-01-22 21:40:54 +0800

    added button

---
+++ react-course/src/components/Button.js
@@ -0,0 +1,20 @@
+const Button = ({ color, text, onClick }) => {
+	// const onClick = e => {
+	// 	console.log(e);
+	// };
+	return (
+		<button
+			onClick={onClick}
+			style={{ backgroundColor: color }}
+			className="btn"
+		>
+			{text}
+		</button>
+	);
+};
+
+Button.defaultProps = {
+	color: "steelblue"
+};
+
+export default Button;
--- react-course/src/components/Header.js
+++ react-course/src/components/Header.js
@@ -1,9 +1,16 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
+	const onClick = () => {
+		console.log("click");
+	};
 	return (
-		<header>
-			<h1 style={headingStyle}>{title}</h1>
+		<header className="header">
+			<h1>{title}</h1>
+			<Button color="green" text="Hello" onClick={onClick} />
+			{/* <Button color="blue" text="Hello 2" />
+			<Button color="red" text="Hello 2" /> */}
 		</header>
 	);
 };
