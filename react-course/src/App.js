4d595d869682bd382cc692fe90053ec866694112
isabelchua <isabelchua86@gmail.com>
2021-01-22 22:47:20 +0800

    task added with icon

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,12 +1,35 @@
+import { useState } from "react";
+
 import "./App.css";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: "food shopping",
+			day: "Feb 5th at 2:30pm",
+			reminder: false
+		},
+		{
+			id: 2,
+			text: "shopping",
+			day: "Feb 5th at 2:30pm",
+			reminder: true
+		},
+		{
+			id: 3,
+			text: "eat",
+			day: "Feb 5th at 2:30pm",
+			reminder: false
+		}
+	]);
+
 	return (
 		<div className="container">
 			<Header />
-			<Tasks />
+			<Tasks tasks={tasks} />
 		</div>
 	);
 }
--- react-course/src/components/Tasks.js
+++ react-course/src/components/Tasks.js
@@ -1,32 +1,11 @@
 // const tasks =
+import Task from "./Task";
 
-import { useState } from "react";
-
-const Tasks = () => {
-	const [tasks, setTasks] = useState([
-		{
-			id: 1,
-			text: "food shopping",
-			day: "Feb 5th at 2:30pm",
-			reminder: false
-		},
-		{
-			id: 2,
-			text: "shopping",
-			day: "Feb 5th at 2:30pm",
-			reminder: true
-		},
-		{
-			id: 3,
-			text: "eat",
-			day: "Feb 5th at 2:30pm",
-			reminder: false
-		}
-	]);
+const Tasks = ({ tasks }) => {
 	return (
 		<>
 			{tasks.map(task => (
-				<h3 key={task.id}>{task.text}</h3>
+				<Task key={task.id} task={task} />
 			))}
 		</>
 	);
