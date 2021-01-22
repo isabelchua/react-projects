756361dcbbebe739d114543f1376952712863c98
isabelchua <isabelchua86@gmail.com>
2021-01-22 22:51:33 +0800

    delete sample

--- react-course/src/components/Tasks.js
+++ react-course/src/components/Tasks.js
@@ -1,11 +1,11 @@
 // const tasks =
 import Task from "./Task";
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete }) => {
 	return (
 		<>
 			{tasks.map(task => (
-				<Task key={task.id} task={task} />
+				<Task key={task.id} task={task} onDelete={onDelete} />
 			))}
 		</>
 	);
