1b3bbe78b17eacb1ab0f86c8090ea7826d756056
isabelchua <isabelchua86@gmail.com>
2021-03-04 19:20:44 +0800

    state

--- react-practice-2-28-2021/src/components/CountButton.js
+++ react-practice-2-28-2021/src/components/CountButton.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CountButton = () => {
-	return <div>hello world</div>;
+	const [count, setCount] = useState(0);
+
+	const handleClick = () => {
+		setCount(count + 1);
+	};
+
+	return (
+		<div>
+			<button onClick={handleClick}>+1</button>
+			<div>{count}</div>
+		</div>
+	);
 };
 
 export default CountButton;
