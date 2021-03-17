08ecad8526168031caaa8f7515f0767f51b0fbcf
isabelchua <isabelchua86@gmail.com>
2021-03-17 11:39:50 -0700

    expense list

--- budget-calc/src/components/ExpenseItem.js
+++ budget-calc/src/components/ExpenseItem.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const ExpenseItem = () => {
-	return <div>expense item</div>;
+const ExpenseItem = ({ expense }) => {
+	const { id, charge, amount } = expense;
+	return (
+		<li>
+			<div className="info">
+				<span className="expense">{charge}</span>
+			</div>
+		</li>
+	);
 };
 
 export default ExpenseItem;
