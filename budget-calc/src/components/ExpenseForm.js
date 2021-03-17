55a33f83d5b2310ea1d466dd71596dc949cdc437
isabelchua <isabelchua86@gmail.com>
2021-03-17 12:27:05 -0700

    form

--- budget-calc/src/components/ExpenseForm.js
+++ budget-calc/src/components/ExpenseForm.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { MdSend } from "react-icons/md";
 
 const ExpenseForm = () => {
-	return <div>expense form</div>;
+	return (
+		<form>
+			<div className="form-center">
+				<div className="form-group">
+					<label htmlFor="charge">charge</label>
+					<input
+						type="text"
+						className="form-control"
+						id="charge"
+						name="charge"
+						placeholder="e.g. rent"
+					/>
+				</div>
+			</div>
+		</form>
+	);
 };
 
 export default ExpenseForm;
