414c0c703e316c09f35cba900f7c57c8f202d4fc
isabelchua <isabelchua86@gmail.com>
2021-03-16 23:55:38 -0700

    setstate

--- budget-calc/src/App.js
+++ budget-calc/src/App.js
@@ -2,8 +2,22 @@ import "./App.css";
 import Alert from "./components/Alert";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseForm from "./components/ExpenseForm";
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
 
+const initialExpenses = [
+	{ id: uuidv4(), charge: "rent", amount: 1500 },
+	{ id: uuidv4(), charge: "car payment", amount: 500 },
+	{ id: uuidv4(), charge: "credit card bill", amount: 1200 }
+];
+
+//console.log(initialExpenses);
 function App() {
+	//console.log(useState());
+	const result = useState(initialExpenses);
+	const expenses = result[0];
+	const setExpenses = result[1];
+	console.log(expenses, setExpenses);
 	return (
 		<div className="App">
 			<Alert />
