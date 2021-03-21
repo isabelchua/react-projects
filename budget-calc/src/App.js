eb837360468aa527833cfb40c4093e12573fe49f
isabelchua <isabelchua86@gmail.com>
2021-03-21 16:17:51 -0700

    total fixed number

--- budget-calc/src/App.js
+++ budget-calc/src/App.js
@@ -66,7 +66,7 @@ function App() {
 				<span className="total">
 					$
 					{expenses.reduce((acc, curr) => {
-						return (acc += curr.amount);
+						return (acc += parseInt(curr.amount));
 					}, 0)}
 				</span>
 			</h1>
