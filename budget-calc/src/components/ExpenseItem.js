60cac96ee349b4db1577fbc661991d28d02f770b
isabelchua <isabelchua86@gmail.com>
2021-03-22 18:13:05 -0700

    cleared items linked

--- budget-calc/src/components/ExpenseItem.js
+++ budget-calc/src/components/ExpenseItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { MdEdit, MdDelete } from "react-icons/md";
 
-const ExpenseItem = ({ expense }) => {
+const ExpenseItem = ({ expense, handleEdit, handleDelete }) => {
 	const { id, charge, amount } = expense;
 	return (
 		<li className="item">
--- budget-calc/src/components/ExpenseList.js
+++ budget-calc/src/components/ExpenseList.js
@@ -2,7 +2,7 @@ import React from "react";
 import Item from "./ExpenseItem";
 import { MdDelete } from "react-icons/md";
 
-const ExpenseList = ({ expenses }) => {
+const ExpenseList = ({ expenses, handleEdit, handleDelete, clearItems }) => {
 	return (
 		<>
 			<ul className="list">
@@ -11,7 +11,7 @@ const ExpenseList = ({ expenses }) => {
 				})}
 			</ul>
 			{expenses.length > 0 && (
-				<button className="btn">
+				<button className="btn" onClick={clearItems}>
 					clear expenses
 					<MdDelete className="btn-icon" />
 				</button>
