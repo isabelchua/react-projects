8c79d2baa0e4dbbebaf89b8eca7fad90774d9b9b
isabelchua <isabelchua86@gmail.com>
2021-03-24 15:23:26 -0700

    localstorage

--- budget-calc/src/App.js
+++ budget-calc/src/App.js
@@ -5,11 +5,15 @@ import ExpenseForm from "./components/ExpenseForm";
 import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 
-const initialExpenses = [
-	{ id: uuidv4(), charge: "rent", amount: 1500 },
-	{ id: uuidv4(), charge: "car payment", amount: 500 },
-	{ id: uuidv4(), charge: "credit card bill", amount: 1200 }
-];
+const initialExpenses = localStorage.getItem("expenses")
+	? JSON.parse(localStorage.getItem("expenses"))
+	: [];
+
+// [
+// 	{ id: uuidv4(), charge: "rent", amount: 1500 },
+// 	{ id: uuidv4(), charge: "car payment", amount: 500 },
+// 	{ id: uuidv4(), charge: "credit card bill", amount: 1200 }
+// ];
 
 function App() {
 	// ******** state values ********
