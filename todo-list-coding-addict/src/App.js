86d3ad356811183cd2aeb6ec7c12a33449f4efba
isabelchua <isabelchua86@gmail.com>
2021-03-26 13:46:24 -0700

    form input

--- todo-list-coding-addict/src/App.js
+++ todo-list-coding-addict/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import TodoList from "./components/TodoList";
+import TodoInput from "./components/TodoInput";
 
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import uuid from "uuid";
 
 function App() {
 	return (
-		<div>
-			<TodoList />
-			hello hello hello hello hello hello hello hello hello hello hello hello
+		<div className="container">
+			<div className="row">
+				<div className="col-10 mx-auto col-md-8 mt-4">
+					<h3 className="text-capitalize text-center">input</h3>
+					<TodoInput />
+					<TodoList />
+				</div>
+			</div>
 		</div>
 	);
 }
--- todo-list-coding-addict/src/components/TodoInput.js
+++ todo-list-coding-addict/src/components/TodoInput.js
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 
 export default class TodoInput extends Component {
 	render() {
-		return <div>hello from todo input</div>;
+		return (
+			<div className="card card-body my-3">
+				<form>
+					<div className="input-group">
+						<div className="input-group-prepend">
+							<div className="input-group-text bg-primary text-white">
+								<i className="fas fa-book"></i>
+							</div>
+						</div>
+						<input type="text" className="form-control" />
+					</div>
+				</form>
+			</div>
+		);
 	}
 }
