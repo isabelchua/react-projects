2b79099106f5cafb2d58cc3ea15cd0db8943efbb
isabelchua <isabelchua86@gmail.com>
2021-03-28 14:29:10 -0700

    ui finished

--- todo-list-coding-addict/src/components/TodoItem.js
+++ todo-list-coding-addict/src/components/TodoItem.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const TodoItem = () => {
-	return <div>todo item</div>;
+	return (
+		<li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+			<h6>title</h6>
+			<div className="todo-icon">
+				<span className="mx-2 text-success">
+					<i className="fas fa-pen"></i>
+				</span>
+				<span className="mx-2 text-danger">
+					<i className="fas fa-trash"></i>
+				</span>
+			</div>
+		</li>
+	);
 };
 
 export default TodoItem;
