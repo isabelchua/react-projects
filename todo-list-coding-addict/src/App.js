48f045fca11766db576ddc3a14f8ba1d9a16a8d5
isabelchua <isabelchua86@gmail.com>
2021-03-28 15:42:38 -0700

    set state class

--- todo-list-coding-addict/src/App.js
+++ todo-list-coding-addict/src/App.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import TodoList from "./components/TodoList";
 import TodoInput from "./components/TodoInput";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import uuid from "uuid";
+import { render } from "react-dom";
 
-function App() {
-	return (
-		<div className="container">
-			<div className="row">
-				<div className="col-10 mx-auto col-md-8 mt-4">
-					<h3 className="text-capitalize text-center">input</h3>
-					<TodoInput />
-					<TodoList />
+class App extends Component {
+	state = {
+		items: [],
+		id: 0,
+		item: "",
+		editItem: false
+	};
+	handleChange = e => {
+		this.setState({
+			item: e.target.value
+		});
+	};
+
+	handleSubmit = e => {};
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-10 mx-auto col-md-8 mt-4">
+						<h3 className="text-capitalize text-center">input</h3>
+						<TodoInput
+							item={this.state.item}
+							handleChange={this.handleChange}
+							handleSubmit={this.handleSubmit}
+						/>
+						<TodoList />
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
