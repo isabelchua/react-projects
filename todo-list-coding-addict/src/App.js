4f7069627903e28ec883978a43a7c8bda0eeecd3
isabelchua <isabelchua86@gmail.com>
2021-03-29 16:21:56 -0700

    edit item input

--- todo-list-coding-addict/src/App.js
+++ todo-list-coding-addict/src/App.js
@@ -50,7 +50,16 @@ class App extends Component {
 	};
 
 	handleEdit = id => {
-		console.log(id);
+		const filteredItems = this.state.items.filter(item => item.id !== id);
+
+		const selectedItem = this.state.items.find(item => item.id === id);
+
+		console.log(selectedItem);
+
+		this.setState({
+			items: filteredItems,
+			item: selectedItem.title
+		});
 	};
 
 	render() {
