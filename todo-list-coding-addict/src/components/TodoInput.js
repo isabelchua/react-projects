8507408277a7439f3f0e3eb9faa5c09cf0ab8007
isabelchua <isabelchua86@gmail.com>
2021-03-29 17:14:18 -0700

    color edit change

--- todo-list-coding-addict/src/components/TodoInput.js
+++ todo-list-coding-addict/src/components/TodoInput.js
@@ -20,7 +20,14 @@ export default class TodoInput extends Component {
 							onChange={handleChange}
 						/>
 					</div>
-					<button type="submit" className="btn btn-block btn-primary mt-3">
+					<button
+						type="submit"
+						className={
+							editItem
+								? "btn btn-block btn-success mt-3"
+								: "btn btn-block btn-primary mt-3"
+						}
+					>
 						{editItem ? "Edit Item" : "Add Item"}
 					</button>
 				</form>
