a2a1ed9973c56dc087cd9ce7935d4d316d3078ea
isabelchua <isabelchua86@gmail.com>
2021-04-06 12:54:19 -0700

    error page

--- beach-resort-class2/src/App.js
+++ beach-resort-class2/src/App.js
@@ -3,18 +3,17 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import Error from "./pages/Error";
 import SingleRoom from "./pages/SingleRoom";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="App">
-			<Route exact path="/" component={Home} />
-			<Route exact path="/rooms/" component={Rooms} />
-			<Route exact path="/single-room" component={SingleRoom} />
-			{/* <Home />
-			<Rooms />
-			<Error />
-			<SingleRoom /> */}
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/rooms/" component={Rooms} />
+				<Route exact path="/rooms/:slug" component={SingleRoom} />
+				<Route component={Error} />
+			</Switch>
 		</div>
 	);
 }
