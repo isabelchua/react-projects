b283089c6214abd5d4de075b11e17137c966db67
isabelchua <isabelchua86@gmail.com>
2021-04-06 13:14:37 -0700

    navbar

--- beach-resort-class2/src/App.js
+++ beach-resort-class2/src/App.js
@@ -5,16 +5,19 @@ import Error from "./pages/Error";
 import SingleRoom from "./pages/SingleRoom";
 import { Route, Link, Switch } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+
 function App() {
 	return (
-		<div className="App">
+		<>
+			<Navbar />
 			<Switch>
 				<Route exact path="/" component={Home} />
 				<Route exact path="/rooms/" component={Rooms} />
 				<Route exact path="/rooms/:slug" component={SingleRoom} />
 				<Route component={Error} />
 			</Switch>
-		</div>
+		</>
 	);
 }
 
