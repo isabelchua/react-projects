83c24e6eae8c8494c2eca3479dce9bce0f96466b
isabelchua <isabelchua86@gmail.com>
2021-04-21 16:02:10 -0700

    wrapped provider

--- beach-resort-class2/src/context.js
+++ beach-resort-class2/src/context.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
-const RoomConext = React.createContext();
+const RoomContext = React.createContext();
 
 //
 
-export default class context extends Component {
+export default class RoomProvider extends Component {
+	state = {};
 	render() {
-		return <div></div>;
+		return (
+			<RoomContext.Provider value="hello">
+				{this.props.children}
+			</RoomContext.Provider>
+		);
 	}
 }
+
+const RoomConsumer = RoomContext.Consumer;
+
+export { RoomProvider, RoomConsumer, RoomContext };
