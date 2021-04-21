c178e527c73aafbc54620b10041a26a7378a174f
isabelchua <isabelchua86@gmail.com>
2021-04-20 19:39:35 -0700

    context

---
+++ beach-resort-class2/src/context.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+const RoomConext = React.createContext();
+
+//
+
+export default class context extends Component {
+	render() {
+		return <div></div>;
+	}
+}
