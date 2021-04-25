c95bd29e03530a520532383a658d7dae5ddb1553
isabelchua <isabelchua86@gmail.com>
2021-04-25 00:44:01 -0700

    getting properties from child

--- beach-resort-class2/src/components/FeaturedRooms.js
+++ beach-resort-class2/src/components/FeaturedRooms.js
@@ -2,17 +2,11 @@ import React, { Component } from "react";
 import { RoomContext } from "../context";
 
 export default class FeaturedRooms extends Component {
-	static contextType = RoomContext;
-	// class RoomProvider extends Component {
-	// 	state = {
-	// 		greeting: 'hello'
-	// 	}
-	// }
+	//static contextType = RoomContext;
 
 	render() {
 		const { name, greeting } = this.context;
-		//const value = this.context;
-		//console.log(value);
+
 		return (
 			<div>
 				{greeting} {name}
--- beach-resort-class2/src/context.js
+++ beach-resort-class2/src/context.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
+import items from "./data";
 const RoomContext = React.createContext();
 
 //
 
 export default class RoomProvider extends Component {
-	state = { greeting: "hello", name: "john" };
+	state = {
+		//greeting: "hello", name: "john"
+		rooms: [],
+		sortedRooms: [],
+		featuredRooms: [],
+		loading: true
+	};
+	// get Data
+
+	componentDidMount() {
+		let rooms = this.formatData(items);
+		console.log(rooms);
+	}
+
+	formatData(items) {
+		let tempItems = items.map(item => {
+			let id = item.sys.id;
+			let images = item.fields.images.map(image => image.fields.file.url);
+
+			let room = { ...item.fields, images, id };
+			return room;
+		});
+		return tempItems;
+	}
+
 	render() {
 		return (
 			<RoomContext.Provider value={{ ...this.state }}>
