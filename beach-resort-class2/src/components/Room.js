e26ed69a85c89a61c76d09922098ec2fa1e0f906
isabelchua <isabelchua86@gmail.com>
2021-05-04 23:24:19 -0700

    proptypes

--- beach-resort-class2/src/components/Room.js
+++ beach-resort-class2/src/components/Room.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import defaultImg from "../images/room-1.jpeg";
+import PropTypes from "prop-types";
 
 export default function Room({ room }) {
-	console.log("rooom" + room);
+	//console.log("rooom" + room);
 	const { name, slug, images, price } = room;
 
 	return (
@@ -14,9 +15,20 @@ export default function Room({ room }) {
 					<h6>${price}</h6>
 					<p>per night</p>
 				</div>
-				<Link to={`/rooms/${slug}`} className="btn-primary room-link" />
+				<Link to={`/rooms/${slug}`} className="btn-primary room-link">
+					Features
+				</Link>
+				<p className="room-info">{name}</p>
 			</div>
-			<p className="room-info">{name}</p>
 		</article>
 	);
 }
+
+Room.propTypes = {
+	room: PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		slug: PropTypes.string.isRequired,
+		images: PropTypes.arrayOf(PropTypes.string).isRequired,
+		price: PropTypes.number.isRequired
+	})
+};
