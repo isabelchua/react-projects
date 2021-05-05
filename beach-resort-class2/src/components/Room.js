9e1ac4e7c4b4fef965f1ac549b6409c4fb9e305d
isabelchua <isabelchua86@gmail.com>
2021-05-04 23:13:50 -0700

    display

--- beach-resort-class2/src/components/Room.js
+++ beach-resort-class2/src/components/Room.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import defaultImg from "../images/room-1.jpeg";
 
 export default function Room({ room }) {
-	console.log("rrooom" + room);
+	console.log("rooom" + room);
 	const { name, slug, images, price } = room;
 
 	return (
 		<article className="room">
-			<img src={images[0]} alt="" />
+			<div className="img-container">
+				<img src={images[0] || defaultImg} alt="single room" />
+				<div className="price-top">
+					<h6>${price}</h6>
+					<p>per night</p>
+				</div>
+				<Link to={`/rooms/${slug}`} className="btn-primary room-link" />
+			</div>
+			<p className="room-info">{name}</p>
 		</article>
 	);
 }
