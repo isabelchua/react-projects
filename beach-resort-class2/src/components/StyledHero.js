598553b7bb1fa4de00d13d30434961422bf4d8d2
isabelchua <isabelchua86@gmail.com>
2021-05-18 14:54:11 -0700

    button

--- beach-resort-class2/src/components/StyledHero.js
+++ beach-resort-class2/src/components/StyledHero.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
+//3.01
+const orange = "#f15025";
+
 const SimpleButton = styled.button`
-	color: red;
+	color: ${orange};
 	background: green;
+	font-size: 3rem;
 `;
 
 export default SimpleButton;
