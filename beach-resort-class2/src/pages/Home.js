fe0219253991954be5a5805cab4176a655257f20
isabelchua <isabelchua86@gmail.com>
2021-05-17 23:52:21 -0700

    button styled

--- beach-resort-class2/src/pages/Home.js
+++ beach-resort-class2/src/pages/Home.js
@@ -4,7 +4,7 @@ import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import Services from "../components/Services";
 import FeaturedRooms from "../components/FeaturedRooms";
-
+import Button from ".../components/StyledHero";
 const Home = () => {
 	return (
 		<>
@@ -20,6 +20,7 @@ const Home = () => {
 			</Hero>
 			<Services />
 			<FeaturedRooms />
+			<Button>hello</Button>
 		</>
 	);
 };
