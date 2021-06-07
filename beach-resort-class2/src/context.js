1bd14729ae7f4589db2fdaaabbb7a3530901e4a9
isabelchua <isabelchua86@gmail.com>
2021-06-07 16:09:34 -0700

    price

--- beach-resort-class2/src/context.js
+++ beach-resort-class2/src/context.js
@@ -27,11 +27,17 @@ export default class RoomProvider extends Component {
 		let rooms = this.formatData(items);
 		//console.log(rooms);
 		let featuredRooms = rooms.filter(room => room.featured === true);
+		let maxPrice = Math.max(...rooms.map(item => item.price));
+		let maxSize = Math.max(...rooms.map(item => item.size));
+
 		this.setState({
 			rooms,
 			featuredRooms,
 			sortedRooms: rooms,
-			loading: false
+			loading: false,
+			price: maxPrice,
+			maxPrice,
+			maxSize
 		});
 	}
 
