99ad6d7d1e17107bfb440c0684e71d29aa3d5dbf
isabelchua <isabelchua86@gmail.com>
2021-06-14 10:28:56 -0700

    context in filter

--- beach-resort-class2/src/components/RoomsFilter.js
+++ beach-resort-class2/src/components/RoomsFilter.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { useContext } from "react";
+import { RoomContext } from "../context";
+import Title from "../components/Title";
 
 export default function RoomsFilter() {
+	const context = useContext(RoomContext);
+	console.log(context);
 	return <div>rooms filter</div>;
 }
