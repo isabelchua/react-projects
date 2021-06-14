1c35abd1ed93209e2b56dd193c2bade851768995
isabelchua <isabelchua86@gmail.com>
2021-06-14 10:35:52 -0700

    filter container

--- beach-resort-class2/src/components/RoomsFilter.js
+++ beach-resort-class2/src/components/RoomsFilter.js
@@ -5,6 +5,37 @@ import Title from "../components/Title";
 
 export default function RoomsFilter() {
 	const context = useContext(RoomContext);
-	console.log(context);
-	return <div>rooms filter</div>;
+	//console.log(context);
+	const {
+		handleChange,
+		type,
+		capacity,
+		price,
+		minPrice,
+		maxPrice,
+		minSize,
+		maxSize,
+		breakfast,
+		pets
+	} = context;
+
+	return (
+		<section className="filter-container">
+			<Title title="search rooms" />
+			<form action="" className="filter-form">
+				{/* select type */}
+				<div className="form-group">
+					<label htmlFor="type">room type</label>
+					<select
+						name="type"
+						id="type"
+						value={type}
+						className="form-control"
+						onChange={handleChange}
+					></select>
+				</div>
+				{/* end select type */}
+			</form>
+		</section>
+	);
 }
