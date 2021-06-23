ff5facbb69a2897b81a0ede8e4876929ef0bb91d
isabelchua <isabelchua86@gmail.com>
2021-06-23 13:46:47 -0700

    filter by price

--- beach-resort-class2/src/context.js
+++ beach-resort-class2/src/context.js
@@ -81,13 +81,25 @@ export default class RoomProvider extends Component {
 			this.state;
 		// all the rooms
 		let tempRooms = [...rooms];
-		// transform
+		// transform values
 		capacity = parseInt(capacity);
+		price = parseInt(capacity);
+
+
 
 		//filter by type
 		if (type !== "all") {
 			tempRooms = tempRooms.filter(room => room.type === type);
 		}
+
+		//filter by capacity
+		if(capacity !== 1){
+			tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+		}
+		//filter by price
+		tempRooms = tempRooms.filter(room => room.price <= price)
+
+		//change state
 		this.setState({ sortedRooms: tempRooms });
 	};
 	//4:20
