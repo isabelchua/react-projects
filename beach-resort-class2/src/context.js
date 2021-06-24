3c6c9b45630ddcdbca300dce7cc7bb803684b93e
isabelchua <isabelchua86@gmail.com>
2021-06-24 11:50:21 -0700

    filter rooms or pets

--- beach-resort-class2/src/context.js
+++ beach-resort-class2/src/context.js
@@ -99,10 +99,10 @@ export default class RoomProvider extends Component {
 		//filter by price
 		tempRooms = tempRooms.filter(room => room.price <= price)
 
-		//filter by size
+		//filter by size 
 		tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
 
-		//filter by breakfast
+		//filter by breakfast 4:55
 		//filter by pets
 
 		//change state
