995c68db6da7d684a8142dbb58489442bf663479
isabelchua <isabelchua86@gmail.com>
2021-06-25 13:04:01 -0700

    header component

--- crash-course-6/src/App.js
+++ crash-course-6/src/App.js
@@ -1,7 +1,11 @@
+import Header from "./components/Header";
 function App() {
 	return (
-		<div className="App">
+		<div className="container">
+			<Header />
+
 			<h1>hello</h1>
+			<h2>aaa</h2>
 		</div>
 	);
 }
