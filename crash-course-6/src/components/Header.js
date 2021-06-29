f87bf0a20d72fa7886532a9ea94959279b24d9d7
isabelchua <isabelchua86@gmail.com>
2021-06-28 23:02:09 -0700

    pass prop

--- crash-course-6/src/components/Header.js
+++ crash-course-6/src/components/Header.js
@@ -2,12 +2,13 @@ import PropTypes from "prop-types";
 import Button from "./Button";
 
 const Header = ({ title }) => {
+	const onClick = () => {
+		console.log("click");
+	};
 	return (
 		<header class="header">
 			<h1>{title}</h1>
-			<Button color="green" text="hello" />
-			<Button color="blue" text="hello 1" />
-			<Button color="red" text="hello 2" />
+			<Button color="green" text="add" onClick={onClick} />
 		</header>
 	);
 };
