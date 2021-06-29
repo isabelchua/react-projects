fb03b9eed29a5cb3b3bce4119ddb75fb31156db5
isabelchua <isabelchua86@gmail.com>
2021-06-28 22:56:34 -0700

    button component

--- crash-course-6/src/components/Header.js
+++ crash-course-6/src/components/Header.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
 	return (
 		<header class="header">
 			<h1>{title}</h1>
-			<button className="btn">Add</button>
+			<Button color="green" text="hello" />
+			<Button color="blue" text="hello 1" />
+			<Button color="red" text="hello 2" />
 		</header>
 	);
 };
