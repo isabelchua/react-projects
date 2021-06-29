f4c8c152e6dbf79416cb5495f1c10b8d303db46b
isabelchua <isabelchua86@gmail.com>
2021-06-28 22:51:36 -0700

    button

--- crash-course-6/src/App.js
+++ crash-course-6/src/App.js
@@ -4,10 +4,7 @@ function App() {
 	// 31:18
 	return (
 		<div className="container">
-			<Header title={"task"} />
-
-			<h1>hello</h1>
-			<h2>aaa</h2>
+			<Header />
 		</div>
 	);
 }
--- crash-course-6/src/components/Header.js
+++ crash-course-6/src/components/Header.js
@@ -2,8 +2,9 @@ import PropTypes from "prop-types";
 
 const Header = ({ title }) => {
 	return (
-		<header>
-			<h1 style={{ color: "red", backgroundColor: "black" }}>{title}</h1>
+		<header class="header">
+			<h1>{title}</h1>
+			<button className="btn">Add</button>
 		</header>
 	);
 };
