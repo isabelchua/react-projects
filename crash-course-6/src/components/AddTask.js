8fdc4a7255730e5fb1f3b71d6cc474202d7709c0
isabelchua <isabelchua86@gmail.com>
2021-07-07 14:48:21 -0700

    add task interface

---
+++ crash-course-6/src/components/AddTask.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const AddTask = () => {
+	return (
+		<form className="add-form">
+			<div className="form-control">
+				<label htmlFor="">Task</label>
+				<input type="text" placeholder="Add Task" />
+			</div>
+			<div className="form-control">
+				<label htmlFor="">Day & Time</label>
+				<input type="text" placeholder="Add Day & Time" />
+			</div>
+			<div className="form-control">
+				<label htmlFor="">Set Reminder</label>
+				<input type="checkbox" placeholder="Add Task" />
+			</div>
+			<input type="submit" value="Save Task" />
+		</form>
+	);
+};
+
+export default AddTask;
