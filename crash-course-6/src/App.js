0838e3d8b7d0a71ab4aeb48309e8b9e908868b60
isabelchua <isabelchua86@gmail.com>
2021-07-07 15:15:46 -0700

    add save on interface

--- crash-course-6/src/App.js
+++ crash-course-6/src/App.js
@@ -30,6 +30,8 @@ function App() {
 	const addTask = task => {
 		//console.log(task);
 		const id = Math.floor(Math.random() * 10000) + 1;
+		const newTask = { id, ...task };
+		setTasks([...tasks, newTask]);
 	};
 
 	// Delete Task
