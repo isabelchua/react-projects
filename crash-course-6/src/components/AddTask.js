c3a557ce5c35354de038b2c005b42388fd49885b
isabelchua <isabelchua86@gmail.com>
2021-07-07 14:54:29 -0700

    styling button and checkbox

--- crash-course-6/src/components/AddTask.js
+++ crash-course-6/src/components/AddTask.js
@@ -11,11 +11,11 @@ const AddTask = () => {
 				<label htmlFor="">Day & Time</label>
 				<input type="text" placeholder="Add Day & Time" />
 			</div>
-			<div className="form-control">
+			<div className="form-control form-control-check">
 				<label htmlFor="">Set Reminder</label>
 				<input type="checkbox" placeholder="Add Task" />
 			</div>
-			<input type="submit" value="Save Task" />
+			<input type="submit" value="Save Task" className="btn btn-block" />
 		</form>
 	);
 };
