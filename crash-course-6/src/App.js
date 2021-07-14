d13df23b787aefd7e96043bb01f1339d6f4714d0
isabelchua <isabelchua86@gmail.com>
2021-07-13 22:41:29 -0700

    fetch tasks from backend

--- crash-course-6/src/App.js
+++ crash-course-6/src/App.js
@@ -9,14 +9,21 @@ function App() {
 	const [tasks, setTasks] = useState([]);
 
 	useEffect(() => {
-		const fetchTasks = async () => {
-			const res = await fetch("http://localhost:5000/tasks");
-			const data = await res.json();
-
-			console.log(data);
+		const getTasks = async () => {
+			const tasksFromServer = await fetchTasks();
+			setTasks(tasksFromServer);
 		};
+		getTasks();
 		fetchTasks();
 	}, []);
+
+	// Fetch Tasks
+	const fetchTasks = async () => {
+		const res = await fetch("http://localhost:5000/tasks");
+		const data = await res.json();
+
+		return data;
+	};
 
 	// Add Task
 	const addTask = task => {
