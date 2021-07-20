80f787298082fd9d2e8c9dd11c1056db7039bd38
isabelchua <isabelchua86@gmail.com>
2021-07-20 16:21:28 -0700

    render about

--- crash-course-6/src/App.js
+++ crash-course-6/src/App.js
@@ -96,18 +96,27 @@ function App() {
 					onAdd={() => setShowAddTask(!showAddTask)}
 					showAdd={showAddTask}
 				/>
-				{showAddTask && <AddTask onAdd={addTask} />}
-				{tasks.length > 0 ? (
-					<Tasks
-						tasks={tasks}
-						onDelete={deleteTask}
-						onToggle={toggleReminder}
-					/>
-				) : (
-					"no tasks to show"
-				)}
+
+				<Route
+					path="/"
+					exact
+					render={props => (
+						<>
+							{showAddTask && <AddTask onAdd={addTask} />}
+							{tasks.length > 0 ? (
+								<Tasks
+									tasks={tasks}
+									onDelete={deleteTask}
+									onToggle={toggleReminder}
+								/>
+							) : (
+								"no tasks to show"
+							)}
+						</>
+					)}
+				/>
 				<Route path="/about" component={About} />
-				<About />
+
 				<Footer />
 			</div>
 		</Router>
