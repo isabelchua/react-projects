f2e6c8a74099bca7d25753179c55e9decbd8a18f
isabelchua <isabelchua86@gmail.com>
2022-01-13 23:41:27 -0800

    link random

--- 1-10-2022-refresher/src/App.js
+++ 1-10-2022-refresher/src/App.js
@@ -4,12 +4,16 @@ import "./App.css";
 function App() {
 	const title = "Welcome!";
 	const likes = 50;
+	const link = "www.google.com";
+	//const person = {name: 'yoshi', age: 30}
 
 	return (
 		<div className="App">
 			<div className="content">
 				<h1>{title}</h1>
 				<p>liked {likes} times</p>
+				<p> {Math.random() * 10}</p>
+				<a href={link}>Google</a>
 			</div>
 		</div>
 	);
