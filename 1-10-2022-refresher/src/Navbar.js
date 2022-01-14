454aebb90252a78cecc2de3661a70d85ff5a558b
isabelchua <isabelchua86@gmail.com>
2022-01-14 00:14:57 -0800

    navbar complete

--- 1-10-2022-refresher/src/Navbar.js
+++ 1-10-2022-refresher/src/Navbar.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Navbar() {
-	return <div></div>;
+	return (
+		<nav className="navbar">
+			<h1>The Dojo Blog</h1>
+			<div className="links">
+				<a href="/">Home</a>
+				<a href="/create">New Blog</a>
+			</div>
+		</nav>
+	);
 }
