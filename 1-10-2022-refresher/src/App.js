188797d21c483256f5ff426953dea7069a1f3945
isabelchua <isabelchua86@gmail.com>
2022-01-13 23:35:37 -0800

    number

--- 1-10-2022-refresher/src/App.js
+++ 1-10-2022-refresher/src/App.js
@@ -3,10 +3,13 @@ import "./App.css";
 
 function App() {
 	const title = "Welcome!";
+	const likes = 50;
+
 	return (
 		<div className="App">
 			<div className="content">
 				<h1>{title}</h1>
+				<p>liked {likes} times</p>
 			</div>
 		</div>
 	);
