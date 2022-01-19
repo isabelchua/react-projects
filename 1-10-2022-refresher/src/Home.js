99bc92effa60ddb9bb53fcbf43068f1d68e304c8
isabelchua <isabelchua86@gmail.com>
2022-01-19 10:40:42 -0800

    changing state

--- 1-10-2022-refresher/src/Home.js
+++ 1-10-2022-refresher/src/Home.js
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
 
-const handleClick = () => {
-	console.log("hello there");
-};
-
-const handleClickAgain = name => {
-	console.log("hello " + name);
-};
-
 const Home = () => {
 	const [name, setName] = useState("isabel");
+	const [age, setAge] = useState("35");
+
+	const handleClick = () => {
+		setName("andrew");
+	};
+
+	const handleClickAgain = name => {};
 
 	return (
 		<div>
 			<div className="home">Homepage </div>
-			<p>{name}</p>
+			<p>{name} is</p>
+			<p>{age} years old</p>
 			<button onClick={handleClick}>Click Me</button>
 			<button onClick={e => handleClickAgain("isabel", e)}>
 				click again
