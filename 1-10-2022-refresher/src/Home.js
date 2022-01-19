7b6943b6a11bd2d5f56e0c82e1b84cc6a25d9366
isabelchua <isabelchua86@gmail.com>
2022-01-19 09:58:22 -0800

    click events

--- 1-10-2022-refresher/src/Home.js
+++ 1-10-2022-refresher/src/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
 
+const handleClick = () => {
+	console.log("hello there");
+};
+
+const handleClickAgain = name => {
+	console.log("hello " + name);
+};
+
 const Home = () => {
 	return (
 		<div>
 			<div className="home">Homepage </div>
+			<button onClick={handleClick}>Click Me</button>
+			<button onClick={e => handleClickAgain("isabel", e)}>
+				click again
+			</button>
 		</div>
 	);
 };
