db898b52972eb8a13386c813b279482f0ef456a6
isabelchua <isabelchua86@gmail.com>
2022-01-18 20:13:05 -0800

    cleaned

--- 1-10-2022-refresher/src/App.js
+++ 1-10-2022-refresher/src/App.js
@@ -1,22 +1,12 @@
-import "./App.css";
 import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-	const title = "Welcome!";
-	const likes = 50;
-	const link = "www.google.com";
-	//const person = {name: 'yoshi', age: 30}
-
 	return (
 		<div className="App">
 			<Navbar />
 			<div className="content">
 				<Home />
-				<h1>{title}</h1>
-				<p>liked {likes} times</p>
-				<p> {Math.random() * 10}</p>
-				<a href={link}>Google</a>
 			</div>
 		</div>
 	);
--- 1-10-2022-refresher/src/Home.js
+++ 1-10-2022-refresher/src/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
 	return (
 		<div>
-			<div className="home">Homepage</div>
+			<div className="home">Homepage </div>
 		</div>
 	);
 };
