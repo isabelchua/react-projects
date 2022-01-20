59774238447feffc23927b173bb3281d4c2c6fdc
isabelchua <isabelchua86@gmail.com>
2022-01-20 10:22:54 -0800

    added delete function

--- refresher-1-20-22/src/App.js
+++ refresher-1-20-22/src/App.js
@@ -24,7 +24,8 @@ function App() {
 
 	// Delete Task
 	const deleteTask = id => {
-		console.log("delete", id);
+		// console.log("delete", id);
+		setTasks(tasks.filter(task => task.id !== id));
 	};
 
 	return (
