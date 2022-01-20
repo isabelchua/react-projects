e7fd8fa1bd591dd4222c55197afa3deba0004d43
isabelchua <isabelchua86@gmail.com>
2022-01-19 17:49:38 -0800

    destructuring

--- 1-10-2022-refresher/src/BlogList.js
+++ 1-10-2022-refresher/src/BlogList.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-const BlogList = props => {
-	const blogs = props.blogs;
-	const title = props.title;
+const BlogList = ({ blogs, title }) => {
+	// const blogs = props.blogs;
+	// const title = props.title;
 
-	console.log(props, blogs);
+	console.log(blogs);
 	return (
 		<div className="bloglist">
 			<h2>{title}</h2>
-			{props.blogs.map(blog => (
+			{blogs.map(blog => (
 				<div className="blogprev" key={blog.id}>
 					<h2>{blog.title}</h2>
 					<p>{blog.body}</p>
