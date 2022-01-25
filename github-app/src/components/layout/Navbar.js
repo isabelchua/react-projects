a570a645cb6e88417044ee98de14c2b1e37337a4
isabelchua <isabelchua86@gmail.com>
2022-01-24 22:43:19 -0800

    styling

--- github-app/src/components/layout/Navbar.js
+++ github-app/src/components/layout/Navbar.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class componentName extends Component {
+	static defaultProps = {
+		title: "Github Finder",
+		icon: "fab fa-github"
+	};
 	render() {
 		return (
 			<nav className="navbar bg-primary">
