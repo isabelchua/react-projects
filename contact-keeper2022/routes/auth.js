a7231ee0ecc51aff5a7de531ff8ab00c49bc59f2
isabelchua <isabelchua86@gmail.com>
2022-01-25 23:02:17 -0800

    added routes

---
+++ contact-keeper2022/routes/auth.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+//@route		GET api/auth
+//@desc		GET logged in user
+//@access 	Private
+router.get("/", (req, res) => {
+	res.send("get logged in user");
+});
+
+//@route		POST api/auth
+//@desc		Auth user & get token
+//@access 	Public
+router.get("/", (req, res) => {
+	res.send("logged in user");
+});
+
+module.exports = router;
