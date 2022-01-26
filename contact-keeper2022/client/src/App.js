53775163ed3c8643da708cb6db4fa5b2953204aa
isabelchua <isabelchua86@gmail.com>
2022-01-26 07:20:07 -0800

    added routes

--- contact-keeper2022/client/src/App.js
+++ contact-keeper2022/client/src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
 import { Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 function App() {
 	return (
 		<Router>
-			<div className="App">
+			<Fragment>
 				<Navbar />
-				test
-			</div>
+				<div className="container">
+					<Routes>
+						<Route exact path="/" element={<Home />} />
+						<Route exact path="/about" element={<About />} />
+					</Routes>
+				</div>
+			</Fragment>
 		</Router>
 	);
 }
