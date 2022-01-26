08fb0468bf4402fa7805f1d436d8ea583990f6d5
isabelchua <isabelchua86@gmail.com>
2022-01-25 19:55:58 -0800

    fetching data from github

--- github-app/src/components/users/Users.js
+++ github-app/src/components/users/Users.js
@@ -2,32 +2,32 @@ import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 class Users extends Component {
-	state = {
-		users: [
-			{
-				id: "1",
-				login: "ead",
-				avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-				html_url: "https://api.github.com/users/mojombo"
-			},
-			{
-				id: "2",
-				login: "test3 ",
-				avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-				html_url: "https://api.github.com/users/mojombo"
-			},
-			{
-				id: "3",
-				login: "hello",
-				avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-				html_url: "https://api.github.com/users/mojombo"
-			}
-		]
-	};
+	// state = {
+	// 	users: [
+	// 		{
+	// 			id: "1",
+	// 			login: "ead",
+	// 			avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+	// 			html_url: "https://api.github.com/users/mojombo"
+	// 		},
+	// 		{
+	// 			id: "2",
+	// 			login: "test3 ",
+	// 			avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+	// 			html_url: "https://api.github.com/users/mojombo"
+	// 		},
+	// 		{
+	// 			id: "3",
+	// 			login: "hello",
+	// 			avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+	// 			html_url: "https://api.github.com/users/mojombo"
+	// 		}
+	// 	]
+	// };
 	render() {
 		return (
 			<div style={userStyle}>
-				{this.state.users.map(user => (
+				{this.props.users.map(user => (
 					<UserItem key={user.id} user={user} />
 				))}
 			</div>
