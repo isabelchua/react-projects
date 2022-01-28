029b067274e0a0e1bfcd66843b92630eddab0c16
isabelchua <isabelchua86@gmail.com>
2022-01-28 12:17:44 -0800

    delete state

--- contact-keeper2022/client/src/context/contact/contactReducer.js
+++ contact-keeper2022/client/src/context/contact/contactReducer.js
@@ -8,12 +8,20 @@ import {
 	CLEAR_FILTER
 } from "../types";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state, action) => {
 	switch (action.type) {
 		case ADD_CONTACT:
 			return {
 				...state,
 				contacts: [...state.contacts, action.payload]
+			};
+		case DELETE_CONTACT:
+			return {
+				...state,
+				contacts: state.contacts.filter(
+					contact => contact.id !== action.payload
+				)
 			};
 		default:
 			return state;
