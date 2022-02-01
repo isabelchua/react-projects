a2c865ded0c54a4b2536124e1d5dd0f36627befa
isabelchua <isabelchua86@gmail.com>
2022-02-01 13:59:18 -0800

    redux setup complete

--- redux-refresh-1-31/src/App.js
+++ redux-refresh-1-31/src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
+	const account = useSelector(state => state.account);
+
+	console.log(account);
+
 	return <div className="App">test</div>;
 }
 
