35c7eaf3ca0636055a630b72bcfdc441e9d6cf32
isabelchua <isabelchua86@gmail.com>
2022-02-01 15:31:27 -0800

    finished

--- redux-refresh-1-31/src/App.js
+++ redux-refresh-1-31/src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state/index";
 
 function App() {
 	const account = useSelector(state => state.account);
+	const dispatch = useDispatch();
+
+	const { depositMoney, withdrawMoney } = bindActionCreators(
+		actionCreators,
+		dispatch
+	);
 
 	return (
 		<div className="App">
 			<h1>{account}</h1>
-			<button>Deposit</button>
-			<button>Withdraw</button>
+			<button onClick={() => depositMoney(1000)}>Deposit</button>
+			<button onClick={() => withdrawMoney(1000)}>Withdraw</button>
 		</div>
 	);
 }
