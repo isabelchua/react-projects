a2b110801e680305d42c9533c58a3c4c3de12929
isabelchua <isabelchua86@gmail.com>
2022-02-02 12:04:22 -0800

    list

--- react-fullcourse-9-hours-dave-gray/src/Content.js
+++ react-fullcourse-9-hours-dave-gray/src/Content.js
@@ -1,23 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Content = () => {
-	const handleNameChange = () => {
-		const names = ["Bob", "Kevin", "Dave"];
-		const int = Math.floor(Math.random() * 3);
-		return names[int];
-	};
+	//const [name, setName] = useState("Isabel");
+	const [items, setItems] = useState([
+		{
+			id: 1,
+			checked: false,
+			item: "Sugar"
+		},
+		{
+			id: 2,
+			checked: true,
+			item: "Chocolate"
+		},
+		{
+			id: 3,
+			checked: false,
+			item: "Beef"
+		}
+	]);
+	//const [count, setCount] = useState(0);
 
-	const handleClick = () => {
-		console.log("click");
-	};
-	const handleClick2 = name => {
-		console.log(name);
+	// const handleNameChange = () => {
+	// 	const names = ["Bob", "Kevin", "Dave"];
+	// 	const int = Math.floor(Math.random() * 3);
+	// 	// return names[int];
+	// 	setName(names[int]);
+	// };
+
+	// const handleClick = () => {
+	// 	console.log("click");
+	// };
+
+	// const handleCount = () => {
+	// 	setCount(count + 1);
+	// };
+
+	// const handleClick2 = name => {
+	// 	console.log(name);
+	// };
+	const handleCheck = id => {
+		const listItems = items.map(item =>
+			item.id === id ? { ...item, checked: !item.checked } : item
+		);
+		setItems(listItems);
 	};
 
 	return (
 		<main>
-			hello {handleNameChange()}!<button onClick={handleClick}>click</button>
-			<button onClick={() => handleClick2(handleNameChange())}>click</button>
+			<ul>
+				{items.map(item => (
+					<li className="item" key={item.id}>
+						<input
+							type="checkbox"
+							onChange={() => handleCheck(item.id)}
+							checked={item.checked}
+						/>
+						<label htmlFor="">{item.item}</label>
+						<FaTrashAlt role="button" tabIndex="0" />
+					</li>
+				))}
+			</ul>
+
+			{/* hello {handleNameChange()}!<button onClick={handleClick}>click</button>
+			hello {handleNameChange}! hello {name}!{count}
+			<button onClick={handleNameChange}>Change Name</button>
+			<button onClick={handleClick}>click</button>
+			<button onClick={handleCount}>count</button>
+			<button onClick={() => handleClick2(handleNameChange())}>
+				change name
+			</button> */}
 		</main>
 	);
 };
