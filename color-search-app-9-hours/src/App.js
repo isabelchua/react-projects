0c5bd441f4700580382024cab0329d98d9bbc742
isabelchua <isabelchua86@gmail.com>
2022-02-03 11:39:40 -0800

    fixed form

--- color-search-app-9-hours/src/App.js
+++ color-search-app-9-hours/src/App.js
@@ -3,17 +3,17 @@ import { useState } from "react";
 
 function App() {
 	const [color, setColor] = useState("green");
-	const onChange = e => {
-		e.preventDefault();
-		setColor(e);
-	};
+	// const onChange = e => {
+	// 	e.preventDefault();
+	// 	setColor(e);
+	// };
 	return (
 		<div className="App">
 			<div className="colorBox" style={{ backgroundColor: color }}></div>
 			<input
 				type="text"
 				value={color}
-				onChange={e => onChange(e.target.value)}
+				onChange={e => setColor(e.target.value)}
 			/>
 		</div>
 	);
