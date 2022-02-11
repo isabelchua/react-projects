8303e1a36218c9ee6e29686fdc49dc56cb7ee53c
isabelchua <isabelchua86@gmail.com>
2022-02-11 13:19:58 -0800

    redone layout to be better for UI

--- 2-10-2022-test/src/App.js
+++ 2-10-2022-test/src/App.js
@@ -4,11 +4,13 @@ import imgDidEat from "./images/dideat.png";
 import imgDidNotEat from "./images/didnoteat.png";
 import food from "./images/food.png";
 import blankBox from "./images/blank-box.png";
+import date from "date-and-time";
 
 function App() {
 	const [text, setText] = useState("");
-	const [didFeed, setDidFeed] = useState("");
+	const [didFeed, setDidFeed] = useState(food);
 	const [didEat, setDidEat] = useState(false);
+	const [dateFed, setDateFed] = useState("");
 
 	const onChange = e => {
 		setText({ ...text, name: e.target.value });
@@ -26,6 +28,8 @@ function App() {
 		} else {
 			setDidFeed(imgDidNotEat);
 		}
+		const now = new Date();
+		setDateFed(date.format(now, "ddd, MMM DD YYYY"));
 		//console.log(didEat);
 	};
 
@@ -34,14 +38,20 @@ function App() {
 	return (
 		<div className="App">
 			<form onSubmit={handleSubmit}>
-				<input type="text" name="name" onChange={onChange} />
-				{text.name}
-				<img src={blankBox} alt="food-img" onClick={onClick} />
-				<img src={food} alt="food-img" onClick={onClick} />
-				<img src={imgDidEat} alt="food-img" onClick={onClick} />
-				<img src={imgDidNotEat} alt="food-img" onClick={onClick} />
+				{/* <input type="text" name="name" onChange={onChange} />
+				{text.name} */}
+				<h1>February 2022</h1>
+				<div>
+					<div className="pet-name">Name</div>
+					<div className="date-fed">Fed on:</div>
+				</div>
+				Avic Versicolor
 				<img src={didFeed} alt="food-img" onClick={onClick} />
-				<button>submit</button>
+				{dateFed}
+				<button>Delete</button>
+				<div>
+					<button>New Feed</button>
+				</div>
 			</form>
 		</div>
 	);
