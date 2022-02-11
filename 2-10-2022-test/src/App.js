97805db10e470fc1a1775c79a0f22fbe0de000b2
isabelchua <isabelchua86@gmail.com>
2022-02-11 12:37:11 -0800

    resources image

--- 2-10-2022-test/src/App.js
+++ 2-10-2022-test/src/App.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import "./App.css";
-import Button from "./Button";
-import TextField from "./TextField";
-import Input from "./TextField";
+import imgDidEat from "./images/dideat.png";
+import imgDidNotEat from "./images/didnoteat.png";
 import food from "./images/food.png";
+import blankBox from "./images/blank-box.png";
 
 function App() {
 	const [text, setText] = useState("");
@@ -16,24 +16,31 @@ function App() {
 
 	const handleSubmit = e => {
 		e.preventDefault();
-		console.log(text);
+		//console.log(text);
 	};
 
 	const onClick = () => {
 		setDidEat(!didEat);
-		console.log(didEat);
+		if (didEat) {
+			setDidFeed(imgDidEat);
+		} else {
+			setDidFeed(imgDidNotEat);
+		}
+		//console.log(didEat);
 	};
 
 	// Add remove food
 
 	return (
 		<div className="App">
-			<Button />
-			<TextField />
 			<form onSubmit={handleSubmit}>
 				<input type="text" name="name" onChange={onChange} />
 				{text.name}
+				<img src={blankBox} alt="food-img" onClick={onClick} />
 				<img src={food} alt="food-img" onClick={onClick} />
+				<img src={imgDidEat} alt="food-img" onClick={onClick} />
+				<img src={imgDidNotEat} alt="food-img" onClick={onClick} />
+				<img src={didFeed} alt="food-img" onClick={onClick} />
 				<button>submit</button>
 			</form>
 		</div>
