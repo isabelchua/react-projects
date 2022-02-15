8315c972c719aa352e18b8a8543323c0317d28a9
isabelchua <isabelchua86@gmail.com>
2022-02-15 13:26:31 -0800

    input

--- todo-list-2-15-2022/src/components/Form.js
+++ todo-list-2-15-2022/src/components/Form.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
-const Form = () => {
-	return <div>Form</div>;
+const Form = ({ input, setInput, todos, setTodos }) => {
+	const onInputChange = e => {
+		setInput(e.target.value);
+	};
+	const onFormSubmit = e => {
+		e.preventDefault();
+		setTodos([...todos, { id: uuidv4(), title: input, completed: false }]);
+		setInput("");
+	};
+	return (
+		<form onSubmit={onFormSubmit}>
+			<input
+				type="text"
+				placeholder="enter todo"
+				className="task-input"
+				value={input}
+				required
+				onChange={onInputChange}
+			/>
+			<button className="button-add" type="submit">
+				Add
+			</button>
+		</form>
+	);
 };
 
 export default Form;
