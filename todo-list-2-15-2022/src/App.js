0768f8d9a1a3e4534980d03464f904ed64ba363c
isabelchua <isabelchua86@gmail.com>
2022-02-15 13:17:51 -0800

    fixed issues with install

--- todo-list-2-15-2022/src/App.js
+++ todo-list-2-15-2022/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-	return <div className="App">test</div>;
+	return (
+		<div className="container">
+			<div className="app-wrapper">
+				<Header />
+			</div>
+		</div>
+	);
 }
 
 export default App;
