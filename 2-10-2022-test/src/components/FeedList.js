8d8c806859e65107c0e528b414335e342581da85
isabelchua <isabelchua86@gmail.com>
2022-02-17 23:13:46 -0800

    edit date

--- 2-10-2022-test/src/components/FeedList.js
+++ 2-10-2022-test/src/components/FeedList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import food from "../images/food.png";
 import imgDidEat from "../images/dideat.png";
 import imgDidNotEat from "../images/didnoteat.png";
@@ -9,21 +9,48 @@ const FeedList = ({ feedList, setFeedList }) => {
 	const [didEat, setDidEat] = useState(false);
 	const [dateFed, setDateFed] = useState("");
 
+	const [editDate, setEditDate] = useState(null);
+	const [changeDate, setChangeDate] = useState("");
+
+	useEffect(() => {
+		if (editDate) {
+			setChangeDate(food.date);
+		} else {
+			setChangeDate("");
+		}
+	}, [setChangeDate, editDate]);
+
 	const handleDelete = ({ id }) => {
 		setFeedList(feedList.filter(feed => feed.id !== id));
 	};
 
-	const onHandleClick = () => {
-		setDidEat(!didEat);
-		if (didEat) {
-			setDidFeed(imgDidEat);
-		} else {
-			setDidFeed(imgDidNotEat);
-		}
-		const now = new Date();
-		setDateFed(date.format(now, "ddd, MMM DD YYYY"));
+	const handleFeed = food => {
+		setFeedList(
+			feedList.map(foodmap => {
+				if (foodmap.id === food.id) {
+					return { ...foodmap, feed: !foodmap.feed };
+				}
+				return foodmap;
+			})
+		);
+
+		// setDidEat(!didEat);
+		// if (didEat) {
+		// 	setDidFeed(imgDidEat);
+		// } else {
+		// 	setDidFeed(imgDidNotEat);
+		// }
+
+		// const now = new Date();
+		// setDateFed(date.format(now, "ddd, MMM DD YYYY"));
 		//console.log(didEat);
 	};
+
+	const handleEditDate = ({ id }) => {
+		const findEditDate = food.find(foodmap => foodmap.id === id);
+		setEditDate(findEditDate);
+	};
+
 	console.log(feedList);
 	return (
 		<div>
@@ -34,12 +61,17 @@ const FeedList = ({ feedList, setFeedList }) => {
 
 			{feedList.map(food => (
 				<div className="row" key={food.id}>
-					<div className="col">test</div>
 					<div className="col">{food.name}</div>
 					<div className="col">
-						<img src={didFeed} alt="food-img" onClick={onHandleClick} />
+						<img
+							src={food.feed ? imgDidEat : imgDidNotEat}
+							alt="food-img"
+							onClick={() => handleFeed(food)}
+						/>
 					</div>
-					<div className="col">{food.date}</div>
+					<div className="col" onClick={() => handleEditDate(food)}>
+						{food.date}
+					</div>
 					<div className="col">
 						<button onClick={() => handleDelete(food)}>delete</button>
 					</div>
