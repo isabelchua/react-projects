d146c2fcaa25b7b4d94a9f1b52525dc9ecbd8578
isabelchua <isabelchua86@gmail.com>
2022-02-25 11:35:59 -0800

    classes

--- javascript/main.js
+++ javascript/main.js
@@ -17,3 +17,14 @@ function addListItem(event) {
 }
 
 document.getElementById("input-todo").addEventListener("keydown", addListItem);
+
+const convertTime = dirtyTime => {
+	let timePeriod = dirtyTime.slice(-2).toLowerCase();
+	let isPM = timePeriod === "pm";
+	let timeNumbers = dirtyTime.slice(0, dirtyTime.length - 2);
+	let [hour, minute] = timeNumbers.split(":").map(item => parseInt(item));
+
+	let convertedHour = hour + (isPM ? 12 : 0);
+	let newHour = convertedHour.padStart(2, "0");
+	return `${newHour}:${minute}`;
+};
--- javascript/vue.js
+++ javascript/vue.js
@@ -1,10 +1,19 @@
 const app = Vue.createApp({
 	data() {
 		return {
+			showBooks: true,
 			title: "my title",
 			name: "isabel",
 			age: 35
 		};
+	},
+	methods: {
+		// changeTitle() {
+		// 	this.title = "test title";
+		// },
+		toggleShowBooks() {
+			this.showBooks = !this.showBooks;
+		}
 	}
 });
 
