b4686b312ea0126877b1aa1a5d9c1e06fd093780
isabelchua <isabelchua86@gmail.com>
2022-03-01 11:54:02 -0800

    fixed error

--- javascript/rectangle.js
+++ javascript/rectangle.js
@@ -1,6 +1,6 @@
 class Rectangle {
 	//Setup
-	constructor (_width, _height, _color) {
+	constructor(_width, _height, _color) {
 		console.log("The Rectangle is being created!");
 
 		this.width = _width;
@@ -11,9 +11,10 @@ class Rectangle {
 	getArea() {
 		return this.width * this.height;
 	}
-	let myRectangle1 = new Rectangle(5,3, "blue");
-	let myRectangle2 = new Rectangle(10, 5, "red")
-
-	console.log(myRectangle1.getArea());
-	console.log(myRectangle2.getArea());
 }
+
+let myRectangle1 = new Rectangle(5, 3, "blue");
+let myRectangle2 = new Rectangle(10, 5, "red");
+
+console.log(myRectangle1.getArea());
+console.log(myRectangle2.getArea());
