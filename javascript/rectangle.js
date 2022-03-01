ea8771bddcbf6da0fb2f5a3811cffa9a0802ed19
isabelchua <isabelchua86@gmail.com>
2022-03-01 12:08:34 -0800

    classes function

--- javascript/rectangle.js
+++ javascript/rectangle.js
@@ -11,6 +11,11 @@ class Rectangle {
 	getArea() {
 		return this.width * this.height;
 	}
+	printDesciption() {
+		console.log(
+			`my width is ${this.width} my height is ${this.height} my color is ${this.color}`
+		);
+	}
 }
 
 let myRectangle1 = new Rectangle(5, 3, "blue");
@@ -18,3 +23,5 @@ let myRectangle2 = new Rectangle(10, 5, "red");
 
 console.log(myRectangle1.getArea());
 console.log(myRectangle2.getArea());
+
+myRectangle1.printDesciption();
