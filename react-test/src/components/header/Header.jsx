152d404d734b04160c95a10f43a27cdd5710e9a4
isabelchua <isabelchua86@gmail.com>
2022-03-03 14:37:27 -0800

    header

--- react-test/src/components/header/Header.jsx
+++ react-test/src/components/header/Header.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<header>
+			<div className="container header__container">
+				<h5>Hello I'm</h5>
+				<h1>Haijia Bintu</h1>
+				.h5.text-light
+			</div>
+		</header>
+	);
 };
 
 export default Header;
