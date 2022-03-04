6574c7c112eb5b9bf21dd6441b77920cffde0589
isabelchua <isabelchua86@gmail.com>
2022-03-04 10:43:20 -0800

    fixed about

--- react-test/src/components/nav/Nav.jsx
+++ react-test/src/components/nav/Nav.jsx
@@ -5,14 +5,24 @@ import { AiOutlineUser } from "react-icons/ai";
 import { BiBook } from "react-icons/bi";
 import { RiServiceLine } from "react-icons/ri";
 import { BiMessageSquareDetail } from "react-icons/bi";
+import { useState } from "react";
 
 const Nav = () => {
+	const [activeNav, setActiveNav] = useState("#");
 	return (
 		<nav>
-			<a href="#">
+			<a
+				href="#"
+				onClick={() => setActiveNav("#")}
+				className={activeNav === "#" ? "active" : ""}
+			>
 				<AiOutlineHome />
 			</a>
-			<a href="#about">
+			<a
+				href="#about"
+				onClick={() => setActiveNav("#about")}
+				className={activeNav === "#about" ? "active" : ""}
+			>
 				<AiOutlineUser />
 			</a>
 			<a href="#experience">
