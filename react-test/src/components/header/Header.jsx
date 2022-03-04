20841defb3d098490293f50a8e74be1dc8f72662
isabelchua <isabelchua86@gmail.com>
2022-03-03 21:30:15 -0800

    header edit

--- react-test/src/components/header/Header.jsx
+++ react-test/src/components/header/Header.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.jpg";
 
 const Header = () => {
 	return (
 		<header>
 			<div className="container header__container">
 				<h5>Hello I'm</h5>
-				<h1>Haijia Bintu</h1>
-				.h5.text-light
+				<h1>Isabel</h1>
+				<h5 className="text-light">Fullstack Developer</h5>
+				<CTA />
+
+				<div className="me">
+					<img src={ME} alt="my profile" />
+				</div>
 			</div>
 		</header>
 	);
