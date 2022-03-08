62ed0cd816bce5003d00f4d96b933c70a4532bec
isabelchua <isabelchua86@gmail.com>
2022-03-08 10:09:14 -0800

    passing down props

--- note-3-7/src/App.js
+++ note-3-7/src/App.js
@@ -1,10 +1,29 @@
 import "./App.css";
+import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
+import { useState } from "react";
 
 function App() {
+	const [notes, setNotes] = useState([
+		{
+			id: nanoid(),
+			text: "this is my note",
+			date: "15/04/2022"
+		},
+		{
+			id: nanoid(),
+			text: "this is my 2nd note",
+			date: "15/04/2022"
+		},
+		{
+			id: nanoid(),
+			text: "this is my 3rd note",
+			date: "15/04/2022"
+		}
+	]);
 	return (
 		<div className="App">
-			<NotesList />
+			<NotesList notes={notes} />
 		</div>
 	);
 }
