d84b885f89034a7cae2fd3a6d3b87667518e6d15
isabelchua <isabelchua86@gmail.com>
2022-03-14 20:05:09 -0700

    todo class

--- class/src/index.js
+++ class/src/index.js
@@ -34,6 +34,23 @@ class HelloMessage extends React.Component {
 	}
 }
 
+class TodoApp extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { items: [], text: "" };
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	render() {
+		return (
+			<div>
+				<h3>TODO</h3>
+			</div>
+		);
+	}
+}
+
 ReactDOM.render(
 	<React.StrictMode>
 		<Timer />
