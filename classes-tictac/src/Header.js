19846be2f7345fd9c201fa0d5bcd852bd38be075
Isabel <isabelchua86@gmail.com>
2022-03-23 23:45:17 -0700

    header

---
+++ classes-tictac/src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div>
+			<h1>Plushisaur</h1>
+			<nav>
+				<li>About</li>
+			</nav>
+		</div>
+	);
+};
+
+export default Header;
