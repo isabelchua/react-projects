f6ebbd970dc1797e3777db148d233b18bf913cba
Isabel <isabelchua86@gmail.com>
2022-03-24 16:07:37 -0700

    class

--- classes-tictac/src/App.js
+++ classes-tictac/src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Header from "./Header";
+import React from "react";
 
 function App() {
-	return (
-		<div className="App">
-			<Header />
-		</div>
-	);
+	class Board extends React.Component {
+		renderSquare(i) {
+			return <Square value={i} />;
+		}
+	}
+
+	class Square extends React.Component {
+		render() {
+			return <button className="square">{this.props.value}</button>;
+		}
+	}
 }
 
 export default App;
