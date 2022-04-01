bca09cc50c3fa3db6372f37ed4c39f649eba374f
Isabel <isabelchua86@gmail.com>
2022-04-01 00:11:41 -0700

    edit body

--- classes-tictac/src/App.js
+++ classes-tictac/src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Header from "./Header";
 import React from "react";
+import Body from "./Body";
 
 class App extends React.Component {
 	render() {
-		return <Header />;
+		return (
+			<>
+				<Header />
+				<Body />
+			</>
+		);
 	}
 }
 
---
+++ classes-tictac/src/Body.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Body = () => {
+	return <div>About FAQ Team</div>;
+};
+
+export default Body;
