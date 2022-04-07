caea195eec476fb9cd2f4521b4eda686f0e1eafe
isabelchua <isabelchua86@gmail.com>
2022-04-06 18:27:07 -0700

    button components

--- test-3-9/src/App.js
+++ test-3-9/src/App.js
@@ -36,7 +36,19 @@ function App() {
 			</div>
 			<button className="span-two">AC</button>
 			<button>DEL</button>
-			<DigitButton digit="/" dispatch={dispatch} />
+			<OperationButton operation="/" dispatch={dispatch} />
+			<DigitButton digit="1" dispatch={dispatch} />
+			<DigitButton digit="2" dispatch={dispatch} />
+			<DigitButton digit="3" dispatch={dispatch} />
+			<OperationButton operation="*" dispatch={dispatch} />
+			<DigitButton digit="4" dispatch={dispatch} />
+			<DigitButton digit="5" dispatch={dispatch} />
+			<DigitButton digit="6" dispatch={dispatch} />
+			<OperationButton operation="+" dispatch={dispatch} />
+			<DigitButton digit="7" dispatch={dispatch} />
+			<DigitButton digit="8" dispatch={dispatch} />
+			<DigitButton digit="9" dispatch={dispatch} />
+			<OperationButton operation="-" dispatch={dispatch} />
 
 			<button>1</button>
 			<button>2</button>
