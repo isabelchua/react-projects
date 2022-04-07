08cc58b38b5bc21225dfc5166a68631f7a6c5a6f
isabelchua <isabelchua86@gmail.com>
2022-04-07 11:52:14 -0700

    operations override

--- test-3-9/src/App.js
+++ test-3-9/src/App.js
@@ -26,6 +26,11 @@ function reducer(state, { type, payload }) {
 			if (state.currentOperand == null && state.previousOperand == null) {
 				return state;
 			}
+
+			if (state.currentOperand == null) {
+				return { ...state, operation: payload.operation };
+			}
+
 			if (state.previousOperand == null) {
 				return {
 					...state,
@@ -35,11 +40,40 @@ function reducer(state, { type, payload }) {
 				};
 			}
 
+			return {
+				...state,
+				previousOperand: evaluate(state),
+				operation: payload.operation,
+				currentOperand: payload.currentOperand
+			};
+
 		case ACTIONS.CLEAR:
 			return {};
 		default:
 			break;
 	}
+}
+
+function evaluate({ currentOperand, previousOperand, operation }) {
+	const prev = parseFloat(previousOperand);
+	const current = parseFloat(currentOperand);
+	if (isNaN(prev) || isNaN(current)) return "";
+	let computation = "";
+	switch (operation) {
+		case "+":
+			computation = prev + current;
+			break;
+		case "-":
+			computation = prev - current;
+			break;
+		case "*":
+			computation = prev * current;
+			break;
+		case "/":
+			computation = prev / current;
+			break;
+	}
+	return computation.toString();
 }
 
 function App() {
@@ -76,7 +110,12 @@ function App() {
 			<OperationButton operation="-" dispatch={dispatch} />
 			<DigitButton digit="." dispatch={dispatch} />
 			<DigitButton digit="0" dispatch={dispatch} />
-			<button className="span-two">=</button>
+			<button
+				className="span-two"
+				onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+			>
+				=
+			</button>
 		</div>
 	);
 }
