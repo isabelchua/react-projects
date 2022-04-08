c4a48d4dc200d34c472f7b53094804e6d40fc40d
isabelchua <isabelchua86@gmail.com>
2022-04-08 12:41:13 -0700

    delete button function

--- test-3-9/src/App.js
+++ test-3-9/src/App.js
@@ -56,6 +56,22 @@ function reducer(state, { type, payload }) {
 
 		case ACTIONS.CLEAR:
 			return {};
+		case ACTIONS.DELETE_DIGIT:
+			if (state.overwrite) {
+				return {
+					...state,
+					overwrite: false,
+					currentOperand: null
+				};
+			}
+			if (state.currentOperand == null) return state;
+			if (state.currentOperand.length === 1) {
+				return { ...state, currentOperand: null };
+			}
+			return {
+				...state,
+				currentOperand: state.currentOperand.slice(0, -1)
+			};
 		case ACTIONS.EVALUATE:
 			if (
 				state.operation == null ||
@@ -116,7 +132,9 @@ function App() {
 			>
 				AC
 			</button>
-			<button>DEL</button>
+			<button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+				DEL
+			</button>
 			<OperationButton operation="/" dispatch={dispatch} />
 			<DigitButton digit="1" dispatch={dispatch} />
 			<DigitButton digit="2" dispatch={dispatch} />
