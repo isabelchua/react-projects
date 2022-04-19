fa197417dbe3a261e0e0395743c70c1850708693
isabelchua <isabelchua86@gmail.com>
2022-04-18 22:53:31 -0700

    blank space removed from being added

--- note-3-7/src/components/AddNote.js
+++ note-3-7/src/components/AddNote.js
@@ -9,7 +9,9 @@ const AddNote = ({ handleAddNote }) => {
 	};
 
 	const handleSaveClick = () => {
-		handleAddNote(noteText);
+		if (noteText.trim().length > 0) {
+			handleAddNote(noteText);
+		}
 	};
 	return (
 		<div className="note new">
