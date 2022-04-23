917f3162e247083bb99b2ca2bf8c892c67336a90
isabelchua <isabelchua86@gmail.com>
2022-04-23 12:11:35 -0700

    list component

---
+++ testing-4-22/src/components/Data.js
@@ -0,0 +1,20 @@
+export const properties = [
+	{
+		id: 1,
+		name: "#1",
+		properties: {
+			mood: "neutral",
+			item: "none",
+			background: "blue"
+		}
+	},
+	{
+		id: 2,
+		name: "#2",
+		properties: {
+			mood: "satisfied",
+			item: "wine",
+			background: "blue"
+		}
+	}
+];
