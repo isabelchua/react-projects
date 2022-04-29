3fc9d34ac8ad25c2d932957103622b23e872a5d7
isabelchua <isabelchua86@gmail.com>
2022-04-28 23:17:39 -0700

    database connected

---
+++ contact-keeper-v2/config/db.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true
+		});
+		console.log("MongoDB connected...");
+	} catch (e) {
+		console.error(e.message);
+		process.exit(1);
+	}
+	// mongoose
+	// 	.connect(db, {
+	// 		useNewUrlParser: true
+	// 	})
+	// 	.then(() => console.log("MongoDB Connected"))
+	// 	.catch(err => {
+	// 		console.error(err.message);
+	// 		process.exit(1);
+	// 	});
+};
+
+module.exports = connectDB;
