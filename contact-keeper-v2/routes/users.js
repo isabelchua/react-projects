991bdf3cbb6bedf5aeb7bb0904ac94b198af8bfd
isabelchua <isabelchua86@gmail.com>
2022-05-02 19:26:51 -0700

    jwt setup

--- contact-keeper-v2/routes/users.js
+++ contact-keeper-v2/routes/users.js
@@ -1,7 +1,9 @@
 const bcrypt = require("bcryptjs");
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 const { body, validationResult, check } = require("express-validator");
+const config = require("config");
 
 const User = require("../models/User");
 
@@ -44,7 +46,25 @@ router.post(
 			user.password = await bcrypt.hash(password, salt);
 
 			await user.save();
-			res.send("User Saved");
+			//res.send("User Saved");
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+			//lets the user log in after registering
+
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				{
+					expiresIn: 99999
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
 		} catch (err) {
 			console.error(err.message);
 			res.status(500).send("Server Error");
