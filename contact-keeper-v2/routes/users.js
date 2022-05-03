73a4638ae326c30802e3c0468f9478ef18913c05
isabelchua <isabelchua86@gmail.com>
2022-05-02 23:16:23 -0700

    login user

--- contact-keeper-v2/routes/users.js
+++ contact-keeper-v2/routes/users.js
@@ -1,6 +1,6 @@
-const bcrypt = require("bcryptjs");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { body, validationResult, check } = require("express-validator");
 const config = require("config");
