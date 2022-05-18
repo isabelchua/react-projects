638eaf0de589b2a074df8127bf99093faa1eb059
isabelchua <isabelchua86@gmail.com>
2022-05-17 23:25:02 -0700

    current edit

--- contact-keeper-v2/client/src/components/contacts/ContactForm.js
+++ contact-keeper-v2/client/src/components/contacts/ContactForm.js
@@ -6,6 +6,19 @@ const ContactForm = () => {
 
 	const { addContact, current } = contactContext;
 
+	useEffect(() => {
+		if (current !== null) {
+			setContact(current);
+		} else {
+			setContact({
+				name: "",
+				email: "",
+				phone: "",
+				type: "personal"
+			});
+		}
+	}, [contactContext, current]);
+
 	const [contact, setContact] = useState({
 		name: "",
 		email: "",
@@ -30,7 +43,9 @@ const ContactForm = () => {
 
 	return (
 		<form onSubmit={onSubmit}>
-			<h2 className="text-primary">Add Contact</h2>
+			<h2 className="text-primary">
+				{current ? "Edit Contact" : "Add Contact"}
+			</h2>
 			<input
 				type="text"
 				placeholder="name"
