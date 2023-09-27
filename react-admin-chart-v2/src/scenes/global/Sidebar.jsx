82b81a6e6524b352a1abcdd3e9273a8c7eb95532
isabelchua <isabelchua86@gmail.com>
2023-09-27 13:10:36 -0700

    fixed routing issues

--- react-admin-chart-v2/src/scenes/global/Sidebar.jsx
+++ react-admin-chart-v2/src/scenes/global/Sidebar.jsx
@@ -1,24 +1,25 @@
 import { useState } from "react";
-import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import "react-pro-sidebar/dist/css/styles.css";
-import { Box, IConButton, Typography, useTheme } from "@mui/material";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 import { tokens } from "../../theme";
-import Home from "@mui/material/Home";
+// import Home from "@mui/material/Home";
 import PeopleOutlinedIcon from "@mui/icons-material/LightMode";
 import ContactsOutlinedIcon from "@mui/icons-material/DarkMode";
 import ReceiptOutlinedIcon from "@mui/icons-material/Notifications";
 import PersonOutlinedIcon from "@mui/icons-material/Person";
 import CalendarTodayIcon from "@mui/icons-material/Search";
-import BarChartOutlinedIcon from "@mui/icons-material/Person";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChart";
 import PieChartOutlinedIcon from "@mui/icons-material/Search";
-import TimelineOutlinedIcon from "@mui/icons-material/Person";
+import TimelineOutlinedIcon from "@mui/icons-material/Timeline";
 import MenuOutlinedIcon from "@mui/icons-material/Search";
 import MapOutlinedIcon from "@mui/icons-material/Search";
+// import Item from ""
 
 const Sidebar = () => {
 	const theme = useTheme();
-	const colors = tokens(theme.palatte.mode);
+	const colors = tokens(theme.palette.mode);
 	const [isCollapsed, setIsCollapsed] = useState(false);
 	const [selected, setSelected] = useState("Dashboard");
 
@@ -105,6 +106,110 @@ const Sidebar = () => {
 							</Box>
 						</Box>
 					)}
+
+					{/* <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+						<Item
+							title="Dashboard"
+							to="/"
+							icon={<HomeOutlinedIcon />}
+							selected={selected}
+							setSelected={setSelected}
+						/>
+
+						<Typography
+							variant="h6"
+							color={colors.grey[300]}
+							sx={{ m: "15px 0 5px 20px" }}
+						>
+							Data
+						</Typography>
+						<Item
+							title="Manage Team"
+							to="/team"
+							icon={<PeopleOutlinedIcon />}
+							selected={selected}
+							setSelected={setSelected}
+						/>
+						<Item
+							title="Contacts Information"
+							to="/contacts"
+							icon={<ContactsOutlinedIcon />}
+							selected={selected}
+							setSelected={setSelected}
+						/>
+						<Item
+							title="Invoices Balances"
+							to="/invoices"
+							icon={<ReceiptOutlinedIcon />}
+							selected={selected}
+							setSelected={setSelected}
+						/>
+
+						<Typography
+							variant="h6"
+							color={colors.grey[300]}
+							sx={{ m: "15px 0 5px 20px" }}
+						>
+							Pages
+						</Typography>
+						<Item
+							title="Profile Form"
+							to="/form"
+							icon={<PersonOutlinedIcon />}
+							selected={selected}
+							setSelected={setSelected}
+						/>
+						<Item
+							title="Calendar"
+							to="/calendar"
+							icon={<CalendarTodayOutlinedIcon />}
+							selected={selected}
+							setSelected={setSelected}
+						/>
+						<Item
+							title="FAQ Page"
+							to="/faq"
+							icon={<HelpOutlineOutlinedIcon />}
+							selected={selected}
+							setSelected={setSelected}
+						/>
+
+						<Typography
+							variant="h6"
+							color={colors.grey[300]}
+							sx={{ m: "15px 0 5px 20px" }}
+						>
+							Charts
+						</Typography>
+						<Item
+							title="Bar Chart"
+							to="/bar"
+							icon={<BarChartOutlinedIcon />}
+							selected={selected}
+							setSelected={setSelected}
+						/>
+						<Item
+							title="Pie Chart"
+							to="/pie"
+							icon={<PieChartOutlineOutlinedIcon />}
+							selected={selected}
+							setSelected={setSelected}
+						/>
+						<Item
+							title="Line Chart"
+							to="/line"
+							icon={<TimelineOutlinedIcon />}
+							selected={selected}
+							setSelected={setSelected}
+						/>
+						<Item
+							title="Geography Chart"
+							to="/geography"
+							icon={<MapOutlinedIcon />}
+							selected={selected}
+							setSelected={setSelected}
+						/>
+					</Box> */}
 				</Menu>
 			</ProSidebar>
 		</Box>
