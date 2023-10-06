8f6469e4837ad0615b1b131b66a42743bd851868
isabelchua <isabelchua86@gmail.com>
2023-10-06 13:42:39 -0700

    form fields added

--- react-admin-chart-v2/src/scenes/form/index.jsx
+++ react-admin-chart-v2/src/scenes/form/index.jsx
@@ -43,7 +43,108 @@ const Form = () => {
 				onSubmit={handleFormSubmit}
 				initialValues={initialValues}
 				validationSchea={userSchema}
-			></Formik>
+			>
+				{({
+					values,
+					errors,
+					touched,
+					handleBlur,
+					handleChange,
+					handleSubmit
+				}) => (
+					<form onSubmit={handleSubmit}>
+						<Box
+							display="grid"
+							gap="30px"
+							gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+							sx={{
+								"& > div": {
+									gridColumn: isNonMobile ? undefined : "span 4"
+								}
+							}}
+						>
+							<Textfield
+								fullWidth
+								variant="filled"
+								type="text"
+								label="First Name"
+								onBlur={handleBlur}
+								onChange={handleChange}
+								value={values.firstName}
+								name="firstName"
+								error={!!touched.firstName && !!errors.firstName}
+								helperText={touched.firstName && errors.firstName}
+								sx={{ gridColumn: "span 2" }}
+							/>
+							<Textfield
+								fullWidth
+								variant="filled"
+								type="text"
+								label="Last Name"
+								onBlur={handleBlur}
+								onChange={handleChange}
+								value={values.lastName}
+								name="lastName"
+								error={!!touched.lastName && !!errors.lastName}
+								helperText={touched.lastName && errors.lastName}
+								sx={{ gridColumn: "span 2" }}
+							/>
+							<Textfield
+								fullWidth
+								variant="filled"
+								type="text"
+								label="Email"
+								onBlur={handleBlur}
+								onChange={handleChange}
+								value={values.email}
+								name="email"
+								error={!!touched.email && !!errors.email}
+								helperText={touched.email && errors.email}
+								sx={{ gridColumn: "span 4" }}
+							/>
+							<Textfield
+								fullWidth
+								variant="filled"
+								type="text"
+								label="Contact Number"
+								onBlur={handleBlur}
+								onChange={handleChange}
+								value={values.contact}
+								name="contact"
+								error={!!touched.contact && !!errors.contact}
+								helperText={touched.contact && errors.contact}
+								sx={{ gridColumn: "span 4" }}
+							/>
+							<Textfield
+								fullWidth
+								variant="filled"
+								type="text"
+								label="Address 1"
+								onBlur={handleBlur}
+								onChange={handleChange}
+								value={values.address1}
+								name="address1"
+								error={!!touched.address1 && !!errors.address1}
+								helperText={touched.address1 && errors.address1}
+								sx={{ gridColumn: "span 4" }}
+							/>
+							<Textfield
+								fullWidth
+								variant="filled"
+								type="text"
+								label="Address 2"
+								onBlur={handleBlur}
+								onChange={handleChange}
+								value={values.address2}
+								name="address2"
+								error={!!touched.address2 && !!errors.address2}
+								helperText={touched.address2 && errors.address2}
+								sx={{ gridColumn: "span 2" }}
+							/>
+						</Box>
+					</form>
+				)}
+			</Formik>
 		</Box>
 	);
 };
