b255be3ce321729cc310a17b287be8f876f71bda
isabelchua <isabelchua86@gmail.com>
2023-10-11 14:48:00 -0700

    added calendar

--- react-admin-chart-v2/src/App.js
+++ react-admin-chart-v2/src/App.js
@@ -14,7 +14,7 @@ import Form from "./scenes/form";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/Geography";
-// import Calendar from "./scenes/Calendar";
+import Calendar from "./scenes/calendar";
 
 function App() {
 	const [theme, colorMode] = useMode();
@@ -34,12 +34,13 @@ function App() {
 							<Route path="/contacts" element={<Contacts />} />
 							<Route path="/invoices" element={<Invoices />} />
 							<Route path="/form" element={<Form />} />
+							<Route path="/calendar" element={<Calendar />} />
 							{/* <Route path="/bar" element={<Bar />} />
 							<Route path="/line" element={<Line />} />
 							<Route path="/pie" element={<Pie />} />
 							<Route path="/faq" element={<FAQ />} />
 							<Route path="/geography" element={<Geography />} />
-							<Route path="/calendar" element={<Calendar />} /> */}
+							 */}
 						</Routes>
 					</main>
 				</div>
