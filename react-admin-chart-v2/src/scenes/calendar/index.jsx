36b005903c3363846d5438d92f5d1694924be085
isabelchua <isabelchua86@gmail.com>
2023-10-11 16:09:58 -0700

    added calendar component and data

--- react-admin-chart-v2/src/scenes/calendar/index.jsx
+++ react-admin-chart-v2/src/scenes/calendar/index.jsx
@@ -84,6 +84,35 @@ const Calendar = () => {
 						))}
 					</List>
 				</Box>
+
+				<Box flex="1 1 100%" ml="15px">
+					<FullCalendar
+						height="75vh"
+						plugins={[
+							dayGridPlugin,
+							timeGridPlugin,
+							interactionPlugin,
+							listPlugin
+						]}
+						headerToolbar={{
+							left: "prev, next today",
+							center: "title",
+							right: "dayGridMonth, timeGridWeek, timeGridDay, listMonth"
+						}}
+						initialView="dayGridMonth"
+						editable={true}
+						selectable={true}
+						selectMirror={true}
+						dayMaxEvents={true}
+						select={handleDateClick}
+						eventClick={handleEventClick}
+						eventsSet={events => setCurrentEvents(events)}
+						initialEvents={[
+							{ id: "1234", title: "All-day event", date: "2022-09-14" },
+							{ id: "4321", title: "Timed Event", date: "2023-03-23" }
+						]}
+					/>
+				</Box>
 			</Box>
 		</Box>
 	);
