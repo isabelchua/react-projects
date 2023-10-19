f92766a94d6586dd0381af1399ba9b8bab870f17
isabelchua <isabelchua86@gmail.com>
2023-10-18 18:28:00 -0700

    dashboard

--- react-admin-chart-v2/src/components/GeographyChart.jsx
+++ react-admin-chart-v2/src/components/GeographyChart.jsx
@@ -1,6 +1,6 @@
-import { useTheme } from "@mui/material";
 import { ResponsiveChoropleth } from "@nivo/geo";
 import { geoFeatures } from "../data/mockGeoFeatures";
+import { useTheme } from "@mui/material";
 import { tokens } from "../theme";
 import { mockGeographyData as data } from "../data/mockData";
 
