8e11c9cfa8519cec910254cbdb8001b1ee79d66f
isabelchua <isabelchua86@gmail.com>
2023-10-19 18:20:14 -0700

    corrected typo

--- react-admin-chart-v2/src/components/StatBox.jsx
+++ react-admin-chart-v2/src/components/StatBox.jsx
@@ -22,19 +22,18 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
 				<Box>
 					<ProgressCircle progress={progress} />
 				</Box>
-
-				<Box display="flex" justifyContent="space-between">
-					<Typography variant="h5" sx={{ color: colors.greenAccent[100] }}>
-						{subtitle}
-					</Typography>
-					<Typography
-						variant="h5"
-						fontStyle="italic"
-						sx={{ color: colors.greenAccent[100] }}
-					>
-						{subtitle}
-					</Typography>
-				</Box>
+			</Box>
+			<Box display="flex" justifyContent="space-between" mt="2px">
+				<Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
+					{subtitle}
+				</Typography>
+				<Typography
+					variant="h5"
+					fontStyle="italic"
+					sx={{ color: colors.greenAccent[600] }}
+				>
+					{increase}
+				</Typography>
 			</Box>
 		</Box>
 	);
